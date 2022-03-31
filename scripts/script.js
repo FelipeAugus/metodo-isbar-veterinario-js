d8ce2b27bdbedfcae5f9574436104061ed4128c4
Felipe Augusto <57400329+FelipeAugus@users.noreply.github.com>
2022-03-30 21:02:36 -0300

    Funcs para Import e Export de registros e Placeholders

--- scripts/script.js
+++ scripts/script.js
@@ -16,7 +16,7 @@ function salvaRegistros() {
         // R
         const recomendacao = obj.getElementsByClassName('recomendacao-text')[0].value;
 
-        console.log(`I - ${nome}  ${infos}\nS - ${situacao}\nB - ${breveHistorico}\nA - ${avaliacao}\nR - ${recomendacao}`)
+        // console.log(`I - ${nome}  ${infos}\nS - ${situacao}\nB - ${breveHistorico}\nA - ${avaliacao}\nR - ${recomendacao}`)
     
         registros[i] = {
             'nome': nome,
@@ -35,15 +35,13 @@ function salvaRegistros() {
 function carregaRegistros() {
     const regStr = localStorage.getItem('registros');
     const registros = JSON.parse(regStr);
-    console.log(registros);
+    // console.log(registros);
     if(registros) preencheRegistros(registros);
 }
 
 function preencheRegistros(registros) {
     Object.keys(registros).forEach(key => {
         const registro = registros[key];
-
-        console.log(registro);
         addRegistro(registro.nome, registro.infos, registro.situacao, registro.historico, registro.avaliacao, registro.recomendacao);
     });
 }
@@ -74,19 +72,19 @@ function addRegistro(nome='', infos='', situacao='', breveHistorico='', avaliaca
                 <div>
                     <div class="mb-2 situacao">
                         <h4>Situação do Fela</h4>
-                        <textarea class="form-control situacao-text" cols="50" rows="5">${situacao}</textarea>
+                        <textarea class="form-control situacao-text" cols="50" rows="5" placeholder="Motivos por quais o paciente necessita de cuidados atualmente, de forma simplificada. Qual a principal preocupação com ele no momento?">${situacao}</textarea>
                     </div>
                     <div class="mb-2 breveHistorico">
                         <h4>Breve Histórico do Fela</h4>
-                        <textarea class="form-control breveHistorico-text" cols="50" rows="5">${breveHistorico}</textarea>
+                        <textarea class="form-control breveHistorico-text" cols="50" rows="5" placeholder="Dados relevantes da história clínica prévia do paciente. Contexto em que ele passou por consulta e foi internado, doenças crônicas anteriormente diagnosticadas, procedimentos importantes pelos quais o animal passou anteriormente.">${breveHistorico}</textarea>
                     </div>
                     <div class="mb-2 avaliacao">
                         <h4>Breve Avaliação do Fela</h4>
-                        <textarea class="form-control avaliacao-text" cols="50" rows="5">${avaliacao}</textarea>
+                        <textarea class="form-control avaliacao-text" cols="50" rows="5" placeholder="Impressão pessoal sobre o paciente e alterações significativas no estado de saúde que ocorreram no último turno. Estratégias que foram necessárias para assessorá-lo (por exemplo: Necessidade de soro glicosado, antieméticos, antitérmicos, anticonvulsivantes, uso do tapete aquecido, oxigenioterapia etc.).">${avaliacao}</textarea>
                     </div>
                     <div class="mb-2 recomendacao">
                         <h4>Recomendações para o Fela</h4>
-                        <textarea class="form-control recomendacao-text" cols="50" rows="5">${recomendacao}</textarea>
+                        <textarea class="form-control recomendacao-text" cols="50" rows="5" placeholder="Sugestões em geral. O que precisa ser feito para a continuidade do seu tratamento? Qual fator merece receber maior atenção? Quais as recomendações em relação à alimentação do paciente? Sugestões para manter o conforto do paciente? Sugestões de exames a serem solicitados? Sugestões de fármacos que devem ser acrescentados?">${recomendacao}</textarea>
                     </div>
                 </div>
             </div>
@@ -97,6 +95,62 @@ function addRegistro(nome='', infos='', situacao='', breveHistorico='', avaliaca
 
 function delRegistro(registro){
     $(`#registro-${registro}`).remove();
+}
+
+function exportarRegistros(){
+    salvaRegistros();
+    const registros = localStorage.getItem('registros');
+
+    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(registros);
+    
+    const downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", `RegistrosISBAR_${Date.now()}.json`);
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+}
+
+function importarRegistros(){
+    const arquivoImportado = document.getElementById('import').files[0];
+
+    if (arquivoImportado.type.match('.json')) {
+        const fileReader = new FileReader();
+        fileReader.onload = function (e) {
+            const conteudoArquivo = JSON.parse(fileReader.result);
+            console.log(conteudoArquivo);
+            if(verificaArquivo(conteudoArquivo)){
+                preencheRegistros(conteudoArquivo);
+            } else {
+                alert("Por favor, entre com um arquivo válido");        
+            }
+        }
+        fileReader.readAsText(arquivoImportado);
+    }
+    else {
+        alert("Por favor, entre com um arquivo válido");
+    }
+
+}
+
+function verificaArquivo(arquivoImportado) {
+    try {
+        let ret = true;
+        Object.keys(arquivoImportado).forEach(key => {
+            const registro = arquivoImportado[key];
+            if(registro.nome === undefined) {ret = false; return;}
+            if(registro.infos === undefined) {ret = false; return;}
+            if(registro.situacao === undefined) {ret = false; return;}
+            if(registro.historico === undefined) {ret = false; return;}
+            if(registro.avaliacao === undefined) {ret = false; return;}
+            if(registro.recomendacao === undefined) {ret = false; return;}
+        });
+        return ret;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }    
 }
 
 function toggle(infos) {
